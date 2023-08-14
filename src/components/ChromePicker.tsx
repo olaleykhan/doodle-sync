9a5f82749e3a0ffa32a03c196b973ce7578f99a4
lekan.dev <realjoy9@gmail.com>
2023-08-14 20:36:44 +0100

    feature: adjust color picker UX

--- src/components/ChromePicker.tsx
+++ src/components/ChromePicker.tsx
@@ -1,13 +1,48 @@
-import React from 'react'
-import { SketchPicker, ColorChangeHandler } from 'react-color';
+import React, { useState } from 'react'
+import { SketchPicker, ColorChangeHandler, ColorResult } from 'react-color';
+import { Box } from '@mui/system';
 
 interface Props {
     color: string
     onChange: ColorChangeHandler
 }
 const ChromePicker: React.FC<Props> = ({ color, onChange }) => {
+    const [pickerOpen, setPickerOpen] = useState(false);
+    const handleColorClick = () => {
+        setPickerOpen(!pickerOpen);
+    };
+
+    const handleColorChangeComplete = (color: ColorResult, event: React.ChangeEvent<HTMLInputElement>) => {
+        onChange(color, event);
+        setPickerOpen(false); // Close the picker after selection
+    };
+
     return (
-        <SketchPicker color={color} onChangeComplete={onChange} />
+        <Box sx={{
+            position: 'relative',
+        }} >
+            <Box sx={{
+                width: '36px',
+                height: '36px',
+                borderRadius: '18px',
+                background: color,
+                cursor: 'pointer',
+
+            }} onClick={handleColorClick} />
+            {
+                pickerOpen && (
+                    <Box sx={{
+                        position: 'absolute',
+                        zIndex: 2,
+                        top: '40px',
+                        right: 0,
+
+                    }} >
+                        <SketchPicker color={color} onChangeComplete={handleColorChangeComplete} />
+                    </Box>
+                )
+            }
+        </Box>
     )
 }
 
