cf0b55c6ddfb1bb6c3c0b3b4dbab30bcc2405142
lekan.dev <realjoy9@gmail.com>
2023-08-27 19:03:53 +0100

    ...

--- src/pages/canva/components/Controls.test.tsx
+++ src/pages/canva/components/Controls.test.tsx
@@ -1,31 +1,26 @@
-import {screen, render} from '@testing-library/react';
+import {render} from '@testing-library/react';
 import Controls from './Controls';
 
 describe('Canvas Control component', () => {
 	render(<Controls
 		lineWidth={1}
-		setLineWidth={() => {}}
+		setLineWidth={() => { }}
 		drawingColor={'#000000'}
-		handleColorChange={() => {}}
-		handleClearCanvas={() => {}}
+		handleColorChange={() => { }}
+		handleClearCanvas={() => { }}
 		isDrawingMode={false}
-		setIsDrawingMode={() => {}}
+		setIsDrawingMode={() => { }}
 	/>);
 
-	//   Test('renders without crashing', () => {
-	//     render(<Controls
-	//         lineWidth={1}
-	//         setLineWidth={() => {}}
-	//         drawingColor={'#000000'}
-	//         handleColorChange={() => {}}
-	//         handleClearCanvas={() => {}}
-	//         isDrawingMode={false}
-	//         setIsDrawingMode={() => {}}
-	//         />)
-	//   });
-
-	// test('various control modes', () => {
-	// 	const lineWidthInput = screen.getByTestId('lineWidthInput');
-	// 	expect(lineWidthInput).toBeInTheDocument();
-	// });
+	test('renders without crashing', () => {
+		render(<Controls
+			lineWidth={1}
+			setLineWidth={() => { }}
+			drawingColor={'#000000'}
+			handleColorChange={() => { }}
+			handleClearCanvas={() => { }}
+			isDrawingMode={false}
+			setIsDrawingMode={() => { }}
+		/>);
+	});
 });
