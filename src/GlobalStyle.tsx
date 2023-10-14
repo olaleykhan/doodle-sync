c14534c1a90f311bd3f090bbb1f127cb12902b4c
lekan.dev <realjoy9@gmail.com>
2023-10-14 16:57:43 +0100

    Migraton:tldraw+vite

---
+++ src/GlobalStyle.tsx
@@ -0,0 +1,48 @@
+// import {FC} from 'react';
+// import {Global, css} from '@emotion/react';
+
+// const GlobalStyle: FC = () => (
+// 	<Global
+// 		styles={css`
+//         html {
+//           font-size: 10px;
+//           @media only screen and (min-width: 1600px) {
+//             font-size: 12px;
+//           }
+//           @media only screen and (min-width: 2000px) {
+//             font-size: 14px;
+//           }
+//           @media only screen and (min-width: 2400px) {
+//             font-size: 16px;
+//           }
+//         }
+//         body {
+//           background-color: #E8E8EA;
+//           margin: 0;
+//           font-family: roboto, sans-serif;
+//         }
+//         /* Hide scrollbar for Chrome, Safari and Opera */
+//         ::-webkit-scrollbar {
+//           width: 8px; 
+//           background-color: #f5f5f7; 
+//         }
+
+//         /* Handle */
+//         ::-webkit-scrollbar-thumb {
+//           background: #bbb; 
+//           border-radius: 10px; 
+//         }
+
+//         /* Handle on hover */
+//         ::-webkit-scrollbar-thumb:hover {
+//           background: #fff; 
+//         }
+
+//         /* Hide scrollbar for Firefox */
+//         scrollbar-width: thin;
+//         scrollbar-color: #888 #f5f5f7; 
+//       `}
+// 	/>
+// );
+
+// export default GlobalStyle;
---
+++ src/theme.ts
@@ -0,0 +1,74 @@
+// import {createTheme, type Theme} from '@mui/material/styles';
+
+// export const theme: Theme = createTheme({
+// 	spacing: (factor: number) => `${factor * 0.8}rem`,
+// 	palette: {
+// 		primary: {
+// 			main: '#639a30',
+// 			contrastText: '#fff',
+// 			light: '#F2FBFA',
+// 			dark: '#00B797',
+// 		},
+// 		success: {
+// 			main: '#00B797',
+// 		},
+// 		grey: {
+// 			// Disable eslint @typescript-eslint/naming-convention
+// 			// eslint-disable-next-line @typescript-eslint/naming-convention
+// 			100: '#ACACAC',
+// 		},
+// 		text: {
+// 			secondary: '#888888',
+// 			primary: '#333333',
+// 			disabled: '#d9d9d9',
+// 		},
+// 		divider: '#DDDDDD',
+// 		background: {
+// 			paper: '#E8E8EA',
+// 			default: '#fff',
+// 		},
+// 	},
+// 	typography: {
+// 		fontFamily: 'roboto, sans-serif',
+// 	},
+// 	components: {
+// 		// eslint-disable-next-line @typescript-eslint/naming-convention
+// 		MuiTypography: {
+// 			styleOverrides: {
+// 				root: {
+// 					fontFamily: 'roboto, sans-serif',
+// 				},
+// 				h4: {
+// 					fontSize: '2.2rem',
+// 					fontWeight: 700,
+// 					lineHeight: '2.4rem',
+// 				},
+// 				subtitle1: {
+// 					fontSize: '1.8rem',
+// 					fontWeight: 400,
+// 					lineHeight: '2.2rem',
+// 				},
+// 				body1: {
+// 					color: '#000000',
+// 					fontSize: '1.6rem',
+// 					fontWeight: 400,
+// 					lineHeight: 'normal ',
+// 				},
+// 				caption: {
+// 					fontSize: '1.6rem',
+// 					fontWeight: 400,
+// 					lineHeight: 'normal ',
+// 					color: '#999999',
+// 				},
+// 			},
+// 		},
+// 		// eslint-disable-next-line @typescript-eslint/naming-convention
+// 		MuiCard: {
+// 			styleOverrides: {
+// 				root: {
+// 					backgroundColor: '#fff',
+// 				},
+// 			},
+// 		},
+// 	},
+// });
