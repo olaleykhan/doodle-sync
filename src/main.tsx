db567e9566a5289660f507fc8340907e851fb369
lekan.dev <realjoy9@gmail.com>
2023-11-13 17:04:35 +0100

    improve UI and themeing

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,30 @@
-import './services/firebase/firebase.ts'
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { ThemeProvider } from '@mui/material/styles';
+import { CssBaseline, StyledEngineProvider } from '@mui/material';
+import { theme } from './theme';
+import GlobalStyle from './GlobalStyle';
+import './index.css';
 import App from './App.tsx'
 import './index.css'
+import './services/firebase/firebase.ts'
 import AuthProvider from '@/contexts/AuthContext'
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <AuthProvider>
-      <App/>
+    
+<AuthProvider>
+      <StyledEngineProvider injectFirst>
+ <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <GlobalStyle />
+       <App/>
+      </ThemeProvider>
+    </StyledEngineProvider>
     </AuthProvider>
+
   </React.StrictMode>,
 )
 
