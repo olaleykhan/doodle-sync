f884989f2826360409f22d32364af0de134d7187
lekan.dev <realjoy9@gmail.com>
2023-11-15 19:14:52 +0100

    init doodle card

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,4 +1,5 @@
 export {default as Home} from './home/Home';
 export {default as Auth} from './auth/Auth';
 export {default as Canvas} from './Canvas';
+export {default as History} from './history/History';
 export {default as ForgotPassword} from './auth/ForgotPassword';
